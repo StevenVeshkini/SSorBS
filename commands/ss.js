580a90e2996fbc75894364797863c1f537039c45
Steven Veshkini <sveshkini@berkeley.edu>
2020-06-12 19:39:47 +0900

    Update SS command

--- commands/ss.js
+++ commands/ss.js
@@ -1,4 +1,5 @@
-console.log()
+const { lambdaURL } = require("../config.json");
+const axios = require('axios').default;
 
 module.exports = {
   name: "ss",
@@ -9,7 +10,21 @@ module.exports = {
   aliases: ["screenshot"],
   usage: "google.com",
   async execute(message, args) {
-    const url = args[0];
+    const webhooks = await message.channel.fetchWebhooks();
+    let hook = webhooks.find(hook => hook.name === 'SSorBS');
+    if (!hook) {
+        hook = await message.channel.createWebhook("SSorBS", {
+          avatar: "https://i.imgur.com/qT9DD0T.png",
+          reason: "Used to post screenshots.",
+        });
+    }
+    const screenshotURL = args[0];
+    // make sure the URL format is ok for Puppeteer.
     await message.channel.send(`Taking a screenshot of ${url}. Please wait a moment.`);
+    await axios.post(lambdaURL, {
+        webhookURL: hook.url,
+        screenshotURL: url,
+        user: message.author.tag
+    });
   },
 };
