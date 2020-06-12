1f029b4ebea3772377fa4ab4f84a207f98094f25
Steven Veshkini <sveshkini@berkeley.edu>
2020-06-12 23:06:17 +0900

    Add format string code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 SSorBS is a Discord bot that lets you take a full page screenshot of any website with a simple command.
 
 Invite SSorBS to your server [here](https://discord.com/oauth2/authorize?client_id=720935079668613150&scope=bot&permissions=536872960).
+
+### How it works
--- commands/ss.js
+++ commands/ss.js
@@ -1,29 +1,37 @@
 const { lambdaURL } = require("../config.json");
 const axios = require('axios').default;
 
+const formatURL = url => {
+  // Checks if there is the 'http(s)://' protocol before the URL, and if not, adds it.
+  if (!/^https?\:\/\//.test(url)) {
+    url = "https://" + url;
+  }
+  return url;
+}
+
 module.exports = {
   name: "ss",
   description: "Take a full-page screenshot of any website.",
-  cooldown: 60,
+  cooldown: 1,
   args: true,
   guildOnly: true,
   aliases: ["screenshot"],
   usage: "google.com",
   async execute(message, args) {
     const webhooks = await message.channel.fetchWebhooks();
-    let hook = webhooks.find(hook => hook.name === 'SSorBS');
+    let hook = webhooks.find(hook => hook.name === 'Screenshot Bot');
     if (!hook) {
-        hook = await message.channel.createWebhook("SSorBS", {
+        hook = await message.channel.createWebhook("Screenshot Bot", {
           avatar: "https://i.imgur.com/qT9DD0T.png",
-          reason: "Used to post screenshots.",
+          reason: "Used to post screenshots to the channel.",
         });
     }
     const screenshotURL = args[0];
-    // make sure the URL format is ok for Puppeteer.
-    await message.channel.send(`Taking a screenshot of ${url}. Please wait a moment.`);
+
+    await message.channel.send(`Taking a screenshot of ${screenshotURL}. Please wait a moment.`);
     await axios.post(lambdaURL, {
         webhookURL: hook.url,
-        screenshotURL: url,
+        screenshotURL: formatURL(screenshotURL),
         user: message.author.tag
     });
   },
